ceebc87be3f8e4ef1bd5914fd0fbde4d1ae5fd12
kolujas <fernandodeibe@gmail.com>
2024-07-02 22:02:28 -0300

    Cambio del titulo en Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <h1 class="text-center text-4xl font-bold py-4">Battle of Pollo</h1>
+      <h1 class="text-center text-4xl font-bold py-4">Battle of Polloeitor</h1>
     </div>
   </template>
   
